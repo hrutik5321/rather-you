df49975c69335cf826f6bcd5361da8d418652cd8
hrutik5321 <hrutikdhumal2003@gamil.com>
2022-02-21 18:04:46 +0530

    redux store removed

--- src/index.js
+++ src/index.js
@@ -8,13 +8,7 @@ import { applyMiddleware, createStore, compose } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 
-const store = createStore(
-  allReducers,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+const store = createStore(allReducers, compose(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <React.StrictMode>
